f3bef817f9a1fda2d2745bddff50f3e4d542032a
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2022-10-11 23:49:41 +0200

    Add isPalindrome

--- functions/string/is.js
+++ functions/string/is.js
@@ -1,1 +1,10 @@
 export const isString = string => typeof string === 'string'
+
+export const isPalindrome = string => {
+  if (typeof string !== 'string') throw new Error('Must provide a string')
+  const regex = /[^A-Za-z0-9]/g
+  const lowered = string.toLowerCase()
+  const replaced = lowered.replace(regex, '')
+  const reverseString = replaced.split('').reverse().join('')
+  return replaced === reverseString
+}
