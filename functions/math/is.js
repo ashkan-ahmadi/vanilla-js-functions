6ade30d14b1fc1a460f539cf4facb2ac5ebcb509
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2022-10-11 17:13:37 +0200

    Add isPrimeNumber function

--- functions/math/is.js
+++ functions/math/is.js
@@ -27,3 +27,19 @@ export const isPositiveNumber = number => {
   if (typeof number !== 'number') throw new Error('Must provide a number type.')
   return number >= 0
 }
+export const isPrimeNumber = number => {
+  if (typeof number !== 'number') throw new Error('Must provide a number type.')
+  if (number === 1) throw new Error('1 is neither prime nor composite number.')
+  if (number < 1) throw new Error('Only positive numbers bigger than 1 can be prime.')
+
+  let isPrime = true
+
+  for (let i = 2; i < number; i++) {
+    if (number % i == 0) {
+      isPrime = false
+      break
+    }
+  }
+
+  return isPrime
+}
