81a0ee6816019271fe85dac40f5222c2694f8ae3
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2022-10-11 16:53:40 +0200

    Remove files no longer needed

--- functions/localStorage.js
+++
@@ -1,7 +0,0 @@
-export const getLocalStorage = name => {
-  localStorage.getItem(name)
-}
-
-export const setLocalStorage = (name, value) => {
-  localStorage.setItem(name, JSON.stringify(value))
-}
