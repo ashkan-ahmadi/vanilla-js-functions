2bfd000a6319b2a2da88e2f59f58bfcea4cdc3d5
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2022-10-14 14:36:36 +0200

    Update removeItemFromArray function

--- functions/array/remove.js
+++ functions/array/remove.js
@@ -6,18 +6,21 @@ export const removeDuplicates = array => {
   return Array.from(new Set(array))
 }
 
-export const removeItemFromArray = (item, array) => {
+export const removeItemFromArray = (item, array, mutate = false) => {
   if (!Array.isArray(array)) throw new Error('Argument must be an array')
 
   const index = array.indexOf(item)
 
-  if (index > -1) {
+  // if item not found in the array (-1), return false
+  if (index === -1) return false
+
+  if (mutate === true) {
     // only splice array when item is found
     array.splice(index, 1) // 2nd parameter means remove one item only
     return array
   }
 
-  return false
+  return array.filter(ITEM => ITEM !== item)
 }
 
 export const removeNonNumberItems = array => {
