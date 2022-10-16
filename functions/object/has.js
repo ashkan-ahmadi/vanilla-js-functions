a67b117c99f7d92d815a94b9dae1da50c142996e
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2022-10-16 23:57:08 +0200

    Add type check to hasKey

--- functions/object/has.js
+++ functions/object/has.js
@@ -1,5 +1,10 @@
+import { isString, isObject } from '../functions.js'
+
 export const hasKey = (key = '', object = {}) => {
-  const keys = Object.keys(object)
+  if (!isObject(object)) throw new Error('Type error: must provide object')
+  if (!isString(key)) throw new Error('Type error: must provide string')
+
+  const keys = Object.keys(object) // returns an array of keys in the object
 
   return keys.includes(key)
 }
