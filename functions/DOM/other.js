3c0689f1af4e98a0f44dadc7c5bc1b4807dac5ba
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2023-03-06 20:52:21 +0100

    Add addParamsToURL() and getFormDataAsObject()

--- functions/DOM/other.js
+++ functions/DOM/other.js
@@ -47,3 +47,37 @@ export const scrollToElementById = (id, options = {}) => {
 export const scrollToTopOfPage = () => {
   window.scrollTo(0, 0)
 }
+
+export const addParamsToURL = (baseURL, params = {}, encode = false) => {
+  /**
+   * Adds params to the end of a URL and returns the complete URL with
+   *
+   * @param {string} baseURL - the URL you want the params to be added to
+   * @param {object} [params={}] - an object with keys and values to be added to the URL as params
+   * @param {bool} [encode=false] - whether to return the final URL encoded or not (default not encoded)
+   *
+   * @return {string} complete URL
+   */
+
+  const url = new URL(baseURL)
+  for (const key in params) {
+    const value = params[key]
+    url.searchParams.append(key, value)
+  }
+
+  return encode ? encodeURIComponent(url.href) : url.href
+}
+
+export const getFormDataAsObject = form => {
+  /**
+   * Returns the form's input values as an object
+   * Note: Only successful form controls are included in a FormData object, i.e. those with a name and not in a disabled state.
+   *
+   * @param {HTMLElement} form - the form whose data you want to get
+   *
+   * @return {object} an object with
+   */
+
+  const formData = new FormData(form)
+  return Object.fromEntries(formData)
+}
